56817d56f491acf897612b8dcbe2fd596c3c2581
Gilad Tsabar <gilad@tsabar.net>
2020-08-24 10:18:03 +0200

    Kata11_Most_sales

---
+++ kata11_Most_sales.js
@@ -0,0 +1,28 @@
+/*
+You work in the best consumer electronics corporation, and your boss wants to find out which three products generate the most revenue. Given 3 lists of the same length like these:
+
+products: ["Computer", "Cell Phones", "Vacuum Cleaner"]
+amounts: [3, 24, 8]
+prices: [199, 299, 399]
+return the three product names with the highest revenue (amount * price).
+
+Note: if multiple products have the same revenue, order them according to their original positions in the input list.
+*/
+
+function top3(products, amounts, prices) {
+  return products.map((item,idx) =>{
+    return {
+      name: item,
+      sum: amounts[idx] * prices[idx],
+      idx: idx
+    };
+  })
+    .sort((a,b)=> b.sum - a.sum || a.idx - b.idx)
+    .slice(0,3)
+    .map(i=> i.name);
+}
+
+console.log(
+  top3(["Computer", "Cell Phones", "Vacuum Cleaner"], [3, 24, 8], [199, 299, 399])
+);
+// //["Cell Phones", "Vacuum Cleaner", "Computer"]
